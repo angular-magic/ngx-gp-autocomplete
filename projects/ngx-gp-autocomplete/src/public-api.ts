a278e07c031536b0005de6aa67b77f9c500c5e27
Dan Cornilov <dancornilov@Dans-MacBook-Pro.local>
2023-05-08 17:44:40 +0800

    feat: export config

--- projects/ngx-gp-autocomplete/src/public-api.ts
+++ projects/ngx-gp-autocomplete/src/public-api.ts
@@ -6,3 +6,4 @@ export * from './lib/ngx-gp-autocomplete.module';
 export * from './lib/ngx-gp-autocomplete.directive';
 export * from './lib/ngx-gp-autocomplete.service';
 export * from './lib/ngx-gp-autocomplete-options';
+export * from './lib/ngx-gp-config';
